f8c8c975c47ba6d53a9e8961b70d37820bffa3b3
RHB <rhb2020@gmail.com>
2024-02-12 22:25:39 +0330

    adding radix ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Box, Container, Grid } from "@radix-ui/themes";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Container>
+        <Grid columns={{ xs: "1", sm: "2", md: "3" }}>
+          <Box>Box 1</Box>
+          <Box>Box 2</Box>
+          <Box>Box 3</Box>
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 export default App;
