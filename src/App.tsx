3a32ff309bd1b7cd7162ee4dceb4f8d0cf076820
RHB <rhb2020@gmail.com>
2024-02-13 01:10:29 +0330

    initial main menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,85 @@
-import { Box, Container, Grid } from "@radix-ui/themes";
+import "./App.css";
+import {
+  Avatar,
+  Box,
+  Card,
+  Container,
+  Flex,
+  Grid,
+  Text,
+} from "@radix-ui/themes";
+
+interface card {
+  title: string;
+  description: string;
+  img_src: string;
+}
 
 const App = () => {
+  const icons_folder = "/Icons/";
+  const cards: card[] = [
+    {
+      title: "انتقال وجه",
+      description: "اطلاعات سقف و کارمزد انتقال وجه",
+      img_src: icons_folder + "transfer.svg",
+    },
+    {
+      title: "کارمزدها",
+      description: "اطلاعات کارمزدها و سر فصل ها",
+      img_src: icons_folder + "commission.svg",
+    },
+    {
+      title: "سپرده ها",
+      description: "اطلاعات سپرده ها و گواهی سپرده",
+      img_src: icons_folder + "deposit.svg",
+    },
+    {
+      title: "تسهیلات",
+      description: "اطلاعات تسهیلات و طرح ها",
+      img_src: icons_folder + "loan.svg",
+    },
+    {
+      title: "اطلاعات بانکی",
+      description: "اطلاعات و تعاریف مورد نیاز",
+      img_src: icons_folder + "information.svg",
+    },
+    {
+      title: "سامانه ها",
+      description: "جستجو و دسترسی به سامانه های مورد نیاز",
+      img_src: icons_folder + "systems.svg",
+    },
+  ];
   return (
     <>
-      <Container>
-        <Grid columns={{ xs: "1", sm: "2", md: "3" }}>
-          <Box>Box 1</Box>
-          <Box>Box 2</Box>
-          <Box>Box 3</Box>
+      <Container key="AppContainer" id="AppContainer">
+        <Grid
+          columns={{ xs: "1", sm: "2" }}
+          gap="5"
+          justify="between"
+          key="MainGrid"
+          id="MainGrid"
+        >
+          {cards.map((card) => (
+            <Card key={card.title} className="MainCard">
+              <Flex className="CardFlex">
+                <Avatar
+                  className="CardAvatar"
+                  src={card.img_src}
+                  size="6"
+                  fallback="BSI"
+                  radius="medium"
+                />
+                <Box className="CardBox">
+                  <Text as="p" className="CardTitle">
+                    {card.title}
+                  </Text>
+                  <Text as="p" className="CardDescription">
+                    {card.description}
+                  </Text>
+                </Box>
+              </Flex>
+            </Card>
+          ))}
         </Grid>
       </Container>
     </>
